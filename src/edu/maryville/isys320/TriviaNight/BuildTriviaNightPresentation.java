e3b6f1ad945d03cb3cc45d0b9c40f5da84c31313
Joe Dent <jdent@maryville.edu>
2017-12-16 13:50:37 -0600

    Cleaning up

--- src/edu/maryville/isys320/TriviaNight/BuildTriviaNightPresentation.java
+++ src/edu/maryville/isys320/TriviaNight/BuildTriviaNightPresentation.java
@@ -2,45 +2,18 @@ package edu.maryville.isys320.TriviaNight;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class BuildTriviaNightPresentation {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		
-		String categories[] = buildExampleCategories();
-		String[] questions = buildExampleQuestions();
-		String[] answers = buildExampleAnswers();
-
+		String fileName = "//Users//joedent//TriviaNight.pptx";
 		SlideShowBuilder builder = new SlideShowBuilder();
-		builder.buildSlideShow(categories, questions, answers, "//Users//joedent//TriviaNight.pptx");
+		QuestionsManager qm = new QuestionsManager("data");
+		
+		builder.buildSlideShow(qm.getCategories(), qm.getQuestions(), qm.getAnswers(), fileName);
 //		builder.printSlideShowInfo("Sample.pptx");
 
-	}
-
-	private static String[] buildExampleAnswers() {
-		String answers[] = new String[100];
-		for (int r = 0; r < 10; r++) {
-			for (int q = 0; q < 10; q++ ) {
-				int index = r * 10 + q;
-				answers[index] = "Answer " + (q+1);
-			}
-		}
-		return answers;
-	}
-
-	private static String[] buildExampleQuestions() {
-		String questions[] = new String[100];
-		for (int r = 0; r < 10; r++) {
-			for (int q = 0; q < 10; q++ ) {
-				int index = r * 10 + q;
-				questions[index] = "Question " + (q+1);
-			}
-		}
-		return questions;
-	}
-
-	private static String[] buildExampleCategories() {
-		return new String[]{"Cat1", "Cat2", "Cat3", "Cat4", "Cat5", "Cat6", "Cat7", "Cat8", "Cat9", "Cat10" };
 	}
 	
 }
---
+++ src/edu/maryville/isys320/TriviaNight/QuestionsManager.java
@@ -0,0 +1,53 @@
+package edu.maryville.isys320.TriviaNight;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class QuestionsManager {
+	public ArrayList<String> categories = new ArrayList<String>();
+	public ArrayList<String> questions = new ArrayList<String>();;
+	public ArrayList<String> answers = new ArrayList<String>();;
+	
+	public QuestionsManager(String categoryFolder) {
+		File fldr = new File(categoryFolder);
+		String[] files = fldr.list();
+		
+		Collections.addAll(categories,files);
+		Collections.sort(categories);
+		for (String fileName : categories) {
+			File f = new File(categoryFolder + "/" + fileName);
+			try {
+				Scanner fs = new Scanner(f);
+				while(fs.hasNextLine()) { // Assume Q, A, Blank Line triplets 
+					questions.add(fs.nextLine());
+					answers.add(fs.nextLine());
+					fs.nextLine();
+				}
+				fs.close();
+
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	public String[] getCategories() {
+		return categories.toArray(new String[categories.size()]);
+	}
+	
+	public String[] getQuestions() {
+		return questions.toArray(new String[questions.size()]);
+	}
+	
+	public String[] getAnswers() {
+		return answers.toArray(new String[answers.size()]);
+	}
+
+}
